602370d88e6be3b16ed89cb7cf2c8d194ff7b205
tam nguyen <ndtam.273@gmail.com>
2020-07-14 17:58:13 +0700

    wrrerwr

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import './adaptive_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTransactionHandler;
@@ -85,20 +86,7 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMMMMd().format(_selectedDate)}',
                       ),
                     ),
-                    Platform.isIOS
-                        ? CupertinoButton(
-                            child: Text('Choose Date',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            onPressed: _presentDatePicker,
-                          )
-                        : FlatButton(
-                            textColor: Theme.of(context).primaryColor,
-                            child: Text(
-                              'Choose Date',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            onPressed: _presentDatePicker,
-                          )
+                  AdaptiveFlatButton('Choose Date', _presentDatePicker),
                   ],
                 ),
               ),
