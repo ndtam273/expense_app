4936a8990fac8aa5407174926dbc2e2b89fb060c
tam nguyen <ndtam.273@gmail.com>
2020-07-16 17:23:49 +0700

    werwerwr

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,16 +7,46 @@ import './adaptive_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTransactionHandler;
-  NewTransaction(this.addTransactionHandler);
+  NewTransaction(this.addTransactionHandler) {
+    print('Constructor NewTransaction Widget');
+  }
 
   @override
-  _NewTransactionState createState() => _NewTransactionState();
+  _NewTransactionState createState() {
+    print('CreateState NewTransaction Widget');
+    return _NewTransactionState();
+  } 
 }
 
 class _NewTransactionState extends State<NewTransaction> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime _selectedDate;
+  
+  _NewTransactionState() {
+    print('Constructor NewTransaction State');
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    print('initState()');
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(NewTransaction oldWidget) {
+    // TODO: implement didUpdateWidget
+    print('didUpdateWidget');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    print('DisposeWidget');
+    super.dispose();
+  }
 
   void _submitData() {
     final enteredTitle = _titleController.text;
